3ae5d25aa162ba70ce9dfa4a57751e74301b9b4b
a510102 <44679115+a510102@users.noreply.github.com>
2020-05-27 16:29:06 +0800

    Updates

---
+++ precache-manifest.1cb387be09d1ef3a2203c03411032884.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d8d7e5feea806f4d017117edbac7c1",
+    "url": "/ibike/index.html"
+  },
+  {
+    "revision": "966a37232839ce23f040",
+    "url": "/ibike/static/css/main.fc831eb0.chunk.css"
+  },
+  {
+    "revision": "e92cd1745d860593725f",
+    "url": "/ibike/static/js/2.3e43d4b1.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/ibike/static/js/2.3e43d4b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "966a37232839ce23f040",
+    "url": "/ibike/static/js/main.6e8869e7.chunk.js"
+  },
+  {
+    "revision": "b78a782f7ecdda5514f8",
+    "url": "/ibike/static/js/runtime-main.943b1483.js"
+  },
+  {
+    "revision": "8ba5e95c749a584b9e5980aa37960ef1",
+    "url": "/ibike/static/media/bikeMark.8ba5e95c.svg"
+  }
+]);
